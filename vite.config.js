f0bfad179d8acc155f4dd22505ec6743489c12c4
REDaHEAD <kyazimov.rostislav@gmail.com>
2025-07-01 21:52:57 +1000

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/scumdb/',  // Название вашего репозитория
   plugins: [react()],
   build: {
     outDir: 'dist',
